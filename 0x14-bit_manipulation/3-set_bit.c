574d292882eece63650c3cc375ea18ad16d924fa
root <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@github.com>
2023-04-18 11:28:12 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,21 +1,28 @@
 #include "main.h"
 
 /**
- * set_bit - sets the value of a bit to 1.
- * at a given index.
- * @n: pointer of an unsigned long int.
- * @index: index of the bit.
- *
- * Return: 1 if it worked, -1 if it didn't.
+ * check_index - checks if the index is within range
+ * @index: the index of the bit to set
+ * Return: 1 if the index is within range, or -1 if an error occurred
+ */
+int check_index(unsigned int index)
+{
+    if (index >= (sizeof(unsigned long int) * 8))
+        return (-1);
+    return (1);
+}
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: a pointer to the number to set the bit in
+ * @index: the index of the bit to set
+ * Return: 1 if it worked, or -1 if an error occurred
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int m;
+    if (check_index(index) == -1)
+        return (-1);
 
-	if (index > 63)
-		return (-1);
-
-	m = 1 << index;
-	*n = (*n | m);
-
-	return (1);
+    *n = (*n | (1 << index));
+    return (1);
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,20 @@
-#ifendef __MAIN_H__
-#define __MAINN_H__
+#include "main.h"
 
-unsigned int binary_to_uint(const char*b);
-void print_binary(unsignd long int n);
-int _putchar(char c);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bit(unsigned long int n, unsigned long in m);
-int get_endianness(void);
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: the number to get the bit from
+ * @index: the index of the bit to get
+ * Return: the value of the bit at index index or -1 if an error occurred
+ */
 
-#endif
+int get_bit(unsigned long int n, unsigned int index)
+{
+    unsigned long int bit_index = sizeof(unsigned long int) * 8;
+
+    if (index >= bit_index)
+    {
+        return (-1);
+    }
+
+    return ((n >> index) & 1);
+}
